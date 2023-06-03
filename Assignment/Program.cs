6d083bcbedacccefa1fccb7d6c14cd1162e7a4e1
Jassi9927 <123195722+Jassi9927@users.noreply.github.com>
2023-06-02 23:13:45 -0400

    Update Program.cs

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,203 +1,211 @@
-//Assignment by Jaspreet Singh A00274134 and Nidhi Ramteke A00250309
+//This assignment is done by Jaspreet Singh A00274134 and Nidhi Ramteke A00250309 
+
 using System;
-using System.Collections.Generic;
-using System.Text;
-// This is the beginning of the class 
+
+// Define the InventoryItem class
 public class InventoryItem
 {
-    public string Name { get; set; }
-    public double Weight { get; set; }
-    public double Volume { get; set; }
+    public double Weight { get; }
+    public double Volume { get; }
 
-    public InventoryItem(string name, double weight, double volume)
+    public InventoryItem(double weight, double volume)
     {
-        Name = name;
         Weight = weight;
         Volume = volume;
     }
 }
 
+// Define the derived classes for each item
 public class Arrow : InventoryItem
 {
-    public Arrow() : base("Arrow", 0.1, 0.05)
-    {
-    }
+    public Arrow() : base(0.1, 0.05) { }
 }
 
 public class Bow : InventoryItem
 {
-    public Bow() : base("Bow", 1, 4)
-    {
-    }
+    public Bow() : base(1.0, 4.0) { }
 }
 
 public class Rope : InventoryItem
 {
-    public Rope() : base("Rope", 1, 1.5)
-    {
-    }
+    public Rope() : base(1.0, 1.5) { }
 }
 
 public class Water : InventoryItem
 {
-    public Water() : base("Water", 2, 3)
-    {
-    }
+    public Water() : base(2.0, 3.0) { }
 }
 
 public class Food : InventoryItem
 {
-    public Food() : base("Food", 1, 0.5)
-    {
-    }
+    public Food() : base(1.0, 0.5) { }
 }
 
 public class Sword : InventoryItem
 {
-    public Sword() : base("Sword", 5, 3)
-    {
-    }
+    public Sword() : base(5.0, 3.0) { }
 }
 
+// Define the Pack class
 public class Pack
 {
-    private List<InventoryItem> items;
-    public int MaxCount { get; }
-    public double MaxVolume { get; }
-    public double MaxWeight { get; }
+    private readonly int maxItems;
+    private readonly double maxWeight;
+    private readonly double maxVolume;
+    private InventoryItem[] items;
+    private int itemCount;
+    private double totalWeight;
+    private double totalVolume;
 
-    public Pack(int maxCount, double maxVolume, double maxWeight)
+    public Pack(int maxItems, double maxWeight, double maxVolume)
     {
-        items = new List<InventoryItem>();
-        MaxCount = maxCount;
-        MaxVolume = maxVolume;
-        MaxWeight = maxWeight;
+        this.maxItems = maxItems;
+        this.maxWeight = maxWeight;
+        this.maxVolume = maxVolume;
+        items = new InventoryItem[maxItems];
     }
 
-    public bool AddItem(InventoryItem item)
+    public bool Add(InventoryItem item)
     {
-        if (items.Count >= MaxCount || GetTotalVolume() + item.Volume > MaxVolume || GetTotalWeight() + item.Weight > MaxWeight)
+        if (itemCount == maxItems || totalWeight + item.Weight > maxWeight || totalVolume + item.Volume > maxVolume)
         {
             return false;
         }
 
-        items.Add(item);
+        items[itemCount++] = item;
+        totalWeight += item.Weight;
+        totalVolume += item.Volume;
+
         return true;
     }
 
-    public double GetTotalVolume()
+    public int ItemCount
     {
-        return items.Sum(item => item.Volume);
+        get { return itemCount; }
     }
 
-    public double GetTotalWeight()
+    public double TotalWeight
     {
-        return items.Sum(item => item.Weight);
+        get { return totalWeight; }
     }
 
-    public override string ToString()
+    public double TotalVolume
     {
-        StringBuilder sb = new StringBuilder();
-        sb.AppendLine($"Pack: maxCount={MaxCount}, maxVolume={MaxVolume}, maxWeight={MaxWeight}");
-        sb.AppendLine("Items:");
-        foreach (var item in items)
-        {
-            sb.AppendLine($"{item.Name}: weight={item.Weight}, volume={item.Volume}");
-        }
-        return sb.ToString();
+        get { return totalVolume; }
+    }
+
+    public int MaxItems
+    {
+        get { return maxItems; }
+    }
+
+    public double MaxWeight
+    {
+        get { return maxWeight; }
+    }
+
+    public double MaxVolume
+    {
+        get { return maxVolume; }
     }
 }
 
-//Providing methods for testing the classes of Pack and InventoryItem
-public static class PackTester
+// Define the program class
+public class Program
 {
-    //This particular method allows user to add items in the pack
-    public static void AddEquipment(Pack pack)
+    public static void Main()
     {
+        Pack pack = new Pack(10 , 30 , 20 );
+
         while (true)
         {
-            //Its a menu item for choices
-            Console.WriteLine("\nChoose an item to add to the pack:");
-            Console.WriteLine("1. Arrow");
-            Console.WriteLine("2. Bow");
-            Console.WriteLine("3. Rope");
-            Console.WriteLine("4. Water");
-            Console.WriteLine("5. Food");
-            Console.WriteLine("6. Sword");
-            Console.WriteLine("0. Exit");
+            Console.WriteLine($"Pack is currently at {pack.ItemCount}/{pack.MaxItems} items, {pack.TotalWeight}/{pack.MaxWeight} weight, and {pack.TotalVolume}/{pack.MaxVolume} volume. What do you want to add?");
+            Console.WriteLine("1 - Arrow");
+            Console.WriteLine("2 - Bow");
+            Console.WriteLine("3 - Rope");
+            Console.WriteLine("4 - Water");
+            Console.WriteLine("5 - Food");
+            Console.WriteLine("6 - Sword");
+            Console.WriteLine("7 - Gather your pack and venture forth");
 
-            int choice = GetChoice(0, 6);
-
-            if (choice == 0)
+            int choice;
+            if (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 7)
             {
-                break;
+                Console.WriteLine("Invalid choice. Please try again.");
+                continue;
             }
 
-            InventoryItem item = null;
-
+            InventoryItem item;
             switch (choice)
             {
-                case 1:
-                    item = new Arrow();
+                case 1: // Arrow
+                    if (pack.Add(new Arrow()))
+                    {
+                        Console.WriteLine("Added Arrow to pack.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pack is full or too heavy/big to add Arrow.");
+                    }
                     break;
-                case 2:
-                    item = new Bow();
+                case 2: // Bow
+                    if (pack.Add(new Bow()))
+                    {
+                        Console.WriteLine("Added Bow to pack.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pack is full or too heavy/big to add Bow.");
+                    }
                     break;
-                case 3:
-                    item = new Rope();
+                case 3: // Rope
+                    if (pack.Add(new Rope()))
+                    {
+                        Console.WriteLine("Added Rope to pack.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pack is full or too heavy/big to add Rope.");
+                    }
                     break;
-                case 4:
-                    item = new Water();
+                case 4: // Water
+                    if (pack.Add(new Water()))
+                    {
+                        Console.WriteLine("Added Water to pack.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pack is full or too heavy/big to add Water.");
+                    }
                     break;
-                case 5:
-                    item = new Food();
+                case 5: // Food
+                    if (pack.Add(new Food()))
+                    {
+                        Console.WriteLine("Added Food to pack.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pack is full or too heavy/big to add Food.");
+                    }
                     break;
-                case 6:
-                    item = new Sword();
+                case 6: // Sword
+                    if (pack.Add(new Sword()))
+                    {
+                        Console.WriteLine("Added Sword to pack.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pack is full or too heavy/big to add Sword.");
+                    }
+                    break;
+                case 7: // Gather your pack and venture forth
+                    Console.WriteLine("Ventureing Forth.");
+                    break;
+                default:
+                    Console.WriteLine("Could not fit this item into the pack.");
                     break;
             }
 
-            if (pack.AddItem(item))
-            {
-                Console.WriteLine("Item added to the pack.");
-            }
-            else
-            {
-                Console.WriteLine("Cannot add item to the pack. Exceeds constraints.");
-            }
-
-            Console.WriteLine(pack);
-        }
-    }
-
-    private static int GetChoice(int min, int max)
-    {
-        int choice;
-        while (!int.TryParse(Console.ReadLine(), out choice) || choice < min || choice > max)
-        {
-            Console.WriteLine("Invalid choice. Please try again.");
-        }
-        return choice;
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        //For the packages it sets the maximum count, weight and volume
-        const int PackMaxItems = 10;
-        const double PackMaxVolume = 20;
-        const double PackMaxWeight = 30;
-        
-        //This is the pack class containing constraints
-        Pack pack = new Pack(PackMaxItems, PackMaxVolume, PackMaxWeight);
-        //Adding items in the pack calling it as a packtester
-        PackTester.AddEquipment(pack);
-        
-        
-//Waiting for the user to interact and will print the message if the program is completed
-        Console.WriteLine("Program finished. Press any key to exit.");
-        Console.ReadKey();
-    }
+        }
+    }
 }
